4b32f6fe00e6187eabe0b6c764543fcecf8dc458
dgaitan <jdavid.gaitan@gmail.com>
2016-10-13 16:54:29 -0600

    mejorando el entorno de trabajo

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer')
+	uglify = require('gulp-uglify')
+	concat = require('gulp-concat');
+	browser = require('browser-sync').create();
 
 //compile sass
 gulp.task('sass', function(){
@@ -10,7 +13,21 @@ gulp.task('sass', function(){
 		.pipe(gulp.dest('./css'));
 });
 
+//minify js
+gulp.task('minify', function(){
+	return gulp.src('./js-vendors/*.js')
+		.pipe(uglify())
+		.pipe(concat('datools.js'))
+		.pipe(gulp.dest('./js'));
+});
+
 //watch sass files
 gulp.task('default', function(){
+	browser.init({
+		server: './'
+	});
+	gulp.watch('./*html').on('change', browser.reload);
+	gulp.watch('./css/*.css').on('change', browser.reload);
+	gulp.watch('./js-vendors/*.js', ['minify']);
 	gulp.watch('./sass/**/*.scss', ['sass']);
 });
